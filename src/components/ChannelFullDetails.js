752a68e291631e309762d1eed925239c4e84b917
Hmin25 <69612620+Hmin25@users.noreply.github.com>
2021-09-21 18:22:12 +0800

    Showing day DONE

--- src/components/ChannelFullDetails.js
+++ src/components/ChannelFullDetails.js
@@ -14,8 +14,10 @@ import Axios from "axios";
 import Header from "./Header";
 
 export default function ChannelFullDetails() {
+  const [currentChannelData, setCurrentChannelData] = useState([]);
+  const history = useHistory();
 
-  let {id, name} = useParams();
+  let { id, name } = useParams();
 
   const DAY = {
     SUNDAY: 0,
@@ -26,11 +28,40 @@ export default function ChannelFullDetails() {
     FRIDAY: 5,
     SATURDAY: 6,
   };
-  const [currentChannelData, setCurrentChannelData] = useState([]);
-  const history = useHistory();
+
+  let day = new Date().getDay();
+  let currentDay;
+  function sorting() {
+    switch (day) {
+      case DAY.SUNDAY:
+        currentDay = "SUN";
+        break;
+      case DAY.MONDAY:
+        currentDay = "MON";
+        break;
+      case DAY.TUESDAY:
+        currentDay = "TUE";
+        break;
+      case DAY.WEDNESDAY:
+        currentDay = "WED";
+        break;
+      case DAY.THURSDAY:
+        currentDay = "THU";
+        break;
+      case DAY.FRIDAY:
+        currentDay = "FRI";
+        break;
+      case DAY.SATURDAY:
+        currentDay = "SAT";
+        break;
+      default:
+        console.log("Not supposed to see this! Day is not within SUN to SAT");
+        break;
+    }
+  }
 
   useEffect(() => {
-    async function getCurrentIdData() {
+    async function getCurrentChannelData() {
       await Axios.get(
         // `https://contenthub-api.eco.astro.com.my/channel/${id}.json  `
         `http://localhost:3000/temp.json`
@@ -39,107 +70,145 @@ export default function ChannelFullDetails() {
           console.log(err);
         })
         .then((res) => {
-          setCurrentChannelData(res.data.response);
+          let data = [];
+          data.push(res.data.response);
+
+          setCurrentChannelData(data);
         });
     }
-    getCurrentIdData();
+    getCurrentChannelData();
   }, []);
 
-  // function displayChannelDetails() {}
+  // useEffect(() => {
+  //   getDayData();
+  // }, []);
 
-  const displayCurrentChannelDetails = () => {
+  const displayCurrentChannelData = () => {
     if (currentChannelData && currentChannelData.length > 0) {
       return currentChannelData.map((currentChannel, index) => (
         <>
-          <Stack pl="26%" flexDir="row" align="baseline">
-            <Button
-              bgColor="transparent"
-              _hover={{ bg: "none", fontWeight: "bold" }}
-              onClick={() => history.push("/")}
-            >
-              content
-            </Button>
-            <Text> {" > "} </Text>
-            <Button
-              cursor="text"
-              fontStyle="normal"
-              bgColor="transparent"
-              _hover={{ bg: "none" }}
-            >
-              {currentChannel.title}
-            </Button>
+          <HStack>
+            <Image src={currentChannel.originalImage} w="70px" h="100%" />
+            <VStack align="self-start" spacing={0}>
+              <Text>CH{currentChannel.stbNumber}</Text>
+              <Text fontWeight="bold">{currentChannel.title}</Text>
+            </VStack>
+          </HStack>
+          <Stack pt="4">
+            <Text>{currentChannel.description}</Text>
           </Stack>
-          <Flex px="32%" pt="3.5%" flexDir="column">
-            <HStack>
-              <Image src={currentChannel.originalImage} w="70px" h="100%" />
-              <VStack align="self-start" spacing={0}>
-                <Text>CH{currentChannel.stbNumber}</Text>
-                <Text fontWeight="bold">{currentChannel.title}</Text>
-              </VStack>
-            </HStack>
-            <Stack pt="4">
-              <Text>{currentChannel.description}</Text>
-            </Stack>
-
-            <HStack fontSize="12px" fontWeight="400" spacing={0} pt="6%">
-              <Button
-                textAlign="left"
-                size="sm"
-                bg="none"
-                _hover={{ bg: "none", fontWeight: "bold" }}
-              >
-                TODAY
-              </Button>
-              <Button
-                size="sm"
-                bg="none"
-                _hover={{ bg: "none", fontWeight: "bold" }}
-              >
-                MON
-              </Button>
-              <Button
-                size="sm"
-                bg="none"
-                _hover={{ bg: "none", fontWeight: "bold" }}
-              >
-                TUE
-              </Button>
-              <Button
-                size="sm"
-                bg="none"
-                _hover={{ bg: "none", fontWeight: "bold" }}
-              >
-                WED
-              </Button>
-              <Button
-                size="sm"
-                bg="none"
-                _hover={{ bg: "none", fontWeight: "bold" }}
-              >
-                THU
-              </Button>
-              <Button
-                size="sm"
-                bg="none"
-                _hover={{ bg: "none", fontWeight: "bold" }}
-              >
-                FRI
-              </Button>
-              <Button
-                size="sm"
-                bg="none"
-                _hover={{ bg: "none", fontWeight: "bold" }}
-              >
-                SAT
-              </Button>
-            </HStack>
-          </Flex>
         </>
       ));
     }
   };
 
-  // return <Box>{currentChannelData && displayCurrentChannelDetails()}</Box>;
-  return( <> <Text>Current ID is: {id}</Text>
-  {currentChannelData && displayCurrentChannelDetails()} </>);
+  const getDays = () => {
+    let dayList = [];
+    if (currentChannelData && currentChannelData.length > 0) {
+      //get all schedule data of current channel
+      let scheduleData = Object.values(currentChannelData[0].schedule);
+
+      return scheduleData.map((schedule) => {
+        if (schedule === scheduleData[0]) {
+          day = day;
+          currentDay = "TODAY";
+        } else {
+          if (day > 5) {
+            day = 0;
+            sorting();
+          } else {
+            day++;
+            sorting();
+          }
+        }
+
+        return (
+          <Button
+            textAlign="left"
+            size="sm"
+            bg="none"
+            _hover={{ bg: "none", fontWeight: "bold" }}
+            // onClick={() => getDayData(dayData={schedule})}
+            // onClick={() => getDayData(dayData={scheduleData[i]})}
+          >
+            {currentDay}
+          </Button>
+        );
+      });
+      //   for (let i = 0; i < scheduleData.length; i++) {
+
+      //     if (i === 0) {
+      //       day = day;
+      //       sorting();
+      //       dayList.push(currentDay);
+      //     } else {
+      //       if (day > 5) {
+      //         day = 0;
+      //         sorting();
+      //         dayList.push(currentDay);
+      //       } else {
+      //         day++;
+      //         sorting();
+      //         dayList.push(currentDay);
+      //       }
+      //     }
+      //   }
+
+      //   dayList.forEach(() => {
+      //     return (
+      //           <Button
+      //             textAlign="left"
+      //             size="sm"
+      //             bg="none"
+      //             _hover={{ bg: "none", fontWeight: "bold" }}
+      //             // onClick={() => getDayData(dayData={schedule})}
+      //             // onClick={() => getDayData(dayData={scheduleData[i]})}
+      //           >
+      //             {dayList}
+      //           </Button>
+      //         );
+      //   })
+    }
+  };
+
+  // function getDayData(dayData) {
+  //   if (dayData && dayData.length > 0) {
+  //     return dayData.map((currentData, index) => (
+  //       <HStack align="start" spacing="8px">
+  //         <Text>on Now</Text>
+  //         <Text>Title</Text>
+  //       </HStack>
+  //     ));
+  //   }
+  // }
+
+  return (
+    <>
+      <Stack pl="26%" flexDir="row" align="baseline">
+        <Button
+          bgColor="transparent"
+          _hover={{ bg: "none", fontWeight: "bold" }}
+          onClick={() => history.push("/")}
+        >
+          content
+        </Button>
+        <Text> {" > "} </Text>
+        <Button
+          cursor="text"
+          fontStyle="normal"
+          bgColor="transparent"
+          _hover={{ bg: "none" }}
+        >
+          {name}
+        </Button>
+      </Stack>
+
+      <Flex px="32%" pt="3%" flexDir="column">
+        {displayCurrentChannelData()}
+        <HStack fontSize="12px" fontWeight="400" spacing={0} pt="6%">
+          {getDays()}
+        </HStack>
+      </Flex>
+    </>
+  );
 }
